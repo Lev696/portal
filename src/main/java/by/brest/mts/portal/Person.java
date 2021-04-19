fdf7b099311d0055011c67a89fb8b98387dcb3ac
lev696 <lev696@gmail.com>
2021-04-19 14:20:57 +0300

    начало

---
+++ src/main/java/by/brest/mts/portal/Person.java
@@ -0,0 +1,4 @@
+package by.brest.mts.portal;
+
+public class Person {
+}
--- src/main/java/by/brest/mts/portal/PortalRestController.java
+++ src/main/java/by/brest/mts/portal/PortalRestController.java
@@ -1,8 +1,16 @@
 package by.brest.mts.portal;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PortalRestController {
+
+    @GetMapping("/person")
+    public String homePage() {
+        System.out.println("hello");
+        String someString = "проверка работоспособности ";
+        return someString + " 5";
+    }
 
 }
