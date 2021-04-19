ab5fe1bf89f5e37df68e0722b33a649904338349
lev696 <lev696@gmail.com>
2021-04-19 16:18:42 +0300

    2021-04-19-16-18

--- src/main/java/by/brest/mts/portal/Person.java
+++ src/main/java/by/brest/mts/portal/Person.java
@@ -1,6 +1,24 @@
 package by.brest.mts.portal;
 
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Person {
-    //
-    //
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    Integer id;
+
+    private String name;
+    private String surname;
+    private int age;
+    private String position;
+
+
+    public Person() {
+    }
+
 }
